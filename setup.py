9e2b602686df5ab92b6e7d85522aac8b45a594ba
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-15 12:07:51 -0300

    custom xadmin setup for Opps Admin application

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-#version_tuple = __import__('xadmin').VERSION
-#version = ".".join([str(v) for v in version_tuple])
-
 setup(
-    name='django-xadmin',
-    version='0.1.12-dev',
-    description='Drop-in replacement of Django admin comes with lots of goodies, fully extensible with plugin support, pretty UI based on Twitter Bootstrap.',
+    name='opps-admin',
+    version='0.1',
+    description='Opps Admin, drop-in replacement of Django admin comes with lots of goodies, fully extensible with plugin support, pretty UI based on Twitter Bootstrap.',
     long_description=open('README.rst').read(),
     author='sshwsfc',
-    author_email='sshwsfc@gmail.com',
-    url='http://www.xadmin.io',
-    download_url='http://github.com/sshwsfc/django-xadmin/archive/0.1.12.dev.zip',
-    packages=['xadmin', 'xadmin.plugins', 'xadmin.templatetags', 'xadmin.views'],
+    url='http://www.oppsproject.org',
+    download_url='http://github.com/opps/opps-admin/tarball/master',
+    packages=find_packages(exclude=('doc', 'docs',)),
     include_package_data=True,
     install_requires=[
         'setuptools',
