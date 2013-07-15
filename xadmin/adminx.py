95a575fef01a73e94c5279a67b841948d0be52ac
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-15 13:59:58 -0300

    Remove all file xadmin, convert to opps-admin

--- xadmin/adminx.py
+++
@@ -1,9 +0,0 @@
-import xadmin
-from xadmin import views
-from models import UserSettings
-from xadmin.layout import *
-
-
-class UserSettingsAdmin(object):
-    model_icon = 'cog'
-xadmin.site.register(UserSettings, UserSettingsAdmin)
